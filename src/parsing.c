498b88e69879eb4421addebf09a0d5ed6fcf9600
Alban Costaz <acostaz@student.42.fr>
2019-06-24 16:35:01 +0200

    zou

--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: acostaz <acostaz@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 11:38:40 by acostaz           #+#    #+#             */
-/*   Updated: 2019/06/24 14:58:45 by acostaz          ###   ########.fr       */
+/*   Updated: 2019/06/24 16:27:25 by acostaz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 void				free_line_and_exit(t_env *e, char **line)
 {
-	free(*line);
+	ft_strdel(line);
 	list_del(e, 4);
 }
 
@@ -40,7 +40,7 @@ void				make_camera(t_env *e)
 {
 	char			*line;
 
-	if (get_next_line(e->fd, &line) == -1)
+	if (get_next_line(e->fd, &line) < 0)
 		ft_error("Invalid file");
 	if (!ft_strequ(line, "camera"))
 	{
@@ -60,18 +60,33 @@ void				make_camera(t_env *e)
 	free(line);
 }
 
-void				make_focus(t_env *e, char **line)
+void				make_focus(t_env *e)
 {
-	free(*line);
-	if (get_next_line(e->fd, line) < 0)
-		ft_error("read error");
-	if (ft_strnequ(*line, "	origin = ", 10))
-		add_coords(&e->focus, *line + 10);
-	else
+	char			*line;
+	int				found;
+
+	found = 0;
+	while (get_next_line(e->fd, &line) > 0)
 	{
-		free(*line);
-		ft_error("Wrong focus input");
+		if (ft_strequ(line, "focus"))
+		{
+			found = 1;
+			free(line);
+			if (get_next_line(e->fd, &line) == -1)
+				ft_error("GNL error");
+			if (ft_strnequ(line, "	origin = ", 10))
+			{
+				add_coords(&e->focus, line + 10);
+				break ;
+			}
+			free(line);
+			ft_error("Wrong focus input");
+		}
+		free(line);
 	}
+	free(line);
+	if (!found)
+		ft_error("Wrong focus input");
 }
 
 void				parsing(t_env *e, char *arg)
@@ -83,12 +98,11 @@ void				parsing(t_env *e, char *arg)
 	e->obj_list = NULL;
 	e->light_list = NULL;
 	make_camera(e);
+	make_focus(e);
 	while (get_next_line(e->fd, &line) > 0)
 	{
 		if (ft_strequ(line, "end_object") || ft_strequ(line, "end_light"))
 			free_line_and_exit(e, &line);
-		if (ft_strequ(line, "focus"))
-			make_focus(e, &line);
 		if (ft_strequ(line, "light"))
 			make_light(e, &line);
 		if (ft_strequ(line, "object"))
